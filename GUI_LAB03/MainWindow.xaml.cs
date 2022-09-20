b134dfa3ba20bd83ce46a5f4b252d7cf4053e6b4
ichiroutaa <gergely.tovizi@gmail.com>
2022-09-20 10:50:21 +0200

    TrooperEditor upgraded

--- GUI_LAB03/MainWindow.xaml.cs
+++ GUI_LAB03/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,39 @@ namespace GUI_LAB03
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        MainWindowViewModel vm;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            vm = new MainWindowViewModel();
+
+            this.DataContext = vm;
+        }
+
+        private void bt_add_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Army.Add(lb_all.SelectedItem as Trooper);
+            l_money.Content = vm.Army.Sum(t => t.value);
+        }
+
+        private void bt_remove_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Army.Remove(lb_army.SelectedItem as Trooper);
+        }
+
+        private void bt_edit_Click(object sender, RoutedEventArgs e)
+        {
+            TrooperEditor te = new TrooperEditor(lb_all.SelectedItem as Trooper);
+            if (te.ShowDialog() == true)
+            {
+            }
+            else
+            {
+
+            }
         }
     }
 }
---
+++ GUI_LAB03/TrooperEditor.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI_LAB03
+{
+    /// <summary>
+    /// Interaction logic for TrooperEditor.xaml
+    /// </summary>
+    public partial class TrooperEditor : Window
+    {
+        private Trooper t;
+        public TrooperEditor(Trooper troop)
+        {
+            InitializeComponent();
+            this.t = troop;
+
+            tb_power.Text = t.Power.ToString();
+            tb_vitality.Text = t.Vitality.ToString();
+            tb_cost.Text = t.Cost.ToString();
+            
+
+        }
+
+        private void Button_click_save(object sender, RoutedEventArgs e)
+        {
+            int newPower = int.Parse(tb_power.Text);
+            int newVitality = int.Parse(tb_vitality.Text);
+            int newCost = int.Parse(tb_cost.Text);
+            t.Save(newPower, newVitality, newCost);
+            this.DialogResult = true;
+        }
+    }
+}
