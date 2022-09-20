e9fd5e92490d12e78659cb4f543c8b5aef3f0ef8
ichiroutaa <gergely.tovizi@gmail.com>
2022-09-20 10:49:51 +0200

    Trooper class extended

--- GUI_LAB03/Trooper.cs
+++ GUI_LAB03/Trooper.cs
@@ -10,13 +10,21 @@ namespace GUI_LAB03
 {
     public class Trooper : INotifyPropertyChanged
     {
-        public string name { get; set; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
 
+        private string name;
         private int power;
         private int vitalty;
         private int cost;
+        public int value 
+        { 
+            get
+            {
+                return power*vitalty*cost;
+            } 
+        }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
@@ -25,25 +33,36 @@ namespace GUI_LAB03
                 handler(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        public void Save(int newPow, int newVit, int newCost)
+        {
+            Power = newPow;
+            Vitality = newVit;
+            Cost = newCost;
+        }
 
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged(); }
+        }
         public int Power
         {
             get { return power; }
-            set { power = value; }
+            set { power = value; OnPropertyChanged(); }
         }
 
 
         public int Vitality
         {
             get { return vitalty; }
-            set { vitalty = value; }
+            set { vitalty = value; OnPropertyChanged(); }
         }
 
 
         public int Cost
         {
             get { return cost; }
-            set { cost = value; }
+            set { cost = value; OnPropertyChanged(); }
         }
 
 
