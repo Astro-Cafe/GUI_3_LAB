6cad911436e84099ef3fa4cef9d13948a995e4ac
ichiroutaa <gergely.tovizi@gmail.com>
2022-09-20 10:50:30 +0200

    Converter created

---
+++ GUI_LAB03/ValueToColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GUI_LAB03
+{
+    public class ValueToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int)value < 4)
+            {
+                return "Red";
+            }
+            else if ((int)value > 5 && (int)value < 7)
+            {
+                return "Yellow";
+            }
+            else
+            {
+                return "LimeGreen";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
